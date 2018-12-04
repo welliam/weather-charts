66d3f5ec8db7c08cc49e1bad4e36313b8aa8154f
Justin Lange <justin.lange@esharesinc.com>
2018-12-03 18:29:05 -0800

    wip

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Weather
+1. set up your database: `sqlite3 weather.db < schema.sql`
+2. set up your environment variables
+  - username: `WEATHER_CHARTS_EMAIL_USERNAME`
+  - to: `WEATHER_CHARTS_EMAIL_TO`
+  - password: `WEATHER_CHARTS_EMAIL_PASSWORD`
+3. set up crontab: e.g. `example-crontab.cron`
+4. build: `stack build`
---
+++ src/Email.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE NamedFieldPuns #-}
+module Email (send) where
+
+import           Data.ByteString    as BS
+import           Data.Text.Encoding as Text
+import           Network.Mail.Mime  (Address, Mail)
+import           Network.Mail.SMTP  as SMTP
+import           System.Environment as Environment
+
+data Credentials = Credentials{username :: String, password :: String, to :: String}
+
+getCredentails :: IO Credentials
+getCredentails = makeCreds
+  <$> Environment.getEnv "WEATHER_CHARTS_EMAIL_USERNAME"
+  <$> Environment.getEnv "WEATHER_CHARTS_EMAIL_TO"
+  <*> Environment.getEnv "WEATHER_CHARTS_EMAIL_PASSWORD"
+  where makeCreds u t p = Credentials{username=u, to=t, password=p}
+
+buildEmail :: Credentials -> BS.ByteString -> Mail
+buildEmail Credentials{username, to} chart = SMTP.simpleMail
+  Address{addressName=Nothing, addressEmail=username}
+  [Address{addressName=Nothing, addressEmail=to}] -- to
+  [] -- cc
+  [] -- bcc
+  "Weather charts" -- subject
+  [SMTP.htmlPart (Text.decodeUtf8 chart)]
+
+sendChart :: BS.ByteString -> IO ()
+sendChart chart = do
+  credentials <- getCredentails
+  let {username, password, to} = credentials
+  buildEmail credentials chart
+  sendMailWithLogin "gmail.com" username password (Text.utf)
